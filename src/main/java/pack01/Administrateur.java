ec378363512241d72e4d25157f9b5389303cb2f1
Ahmedna Bechir <mednib32@gmail.com>
2023-02-05 15:30:44 +0100

    la version du projet qui contient un administrateur par default

---
+++ src/main/java/pack01/Administrateur.java
@@ -0,0 +1,21 @@
+package pack01;
+
+public class Administrateur {
+	
+	private String login = "admin";
+	private String password = "admin";
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
